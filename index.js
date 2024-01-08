6bfc046a45501a65b9de25f9fd3f986f9e3e646b
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2024-01-08 22:58:54 +0000

    Create User model with migration and seeder files and add new dependencies
    A User model is newly added along with corresponding migration and seeder files to handle various user-related tasks. Package.json file is also updated with several new dependencies supporting these additions. A configuration file is also introduced to manage different environment setups including development, test, and production.

---
+++ index.js
@@ -0,0 +1,14 @@
+import {Sequelize} from "sequelize";
+
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: 'database.sqlite'
+});
+
+try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+} catch (error) {
+    console.error('Unable to connect to the database:', error);
+}
+
